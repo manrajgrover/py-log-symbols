edf7d5cc5c3a5993cc5ab6ba12bfcbb4a196601e
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-09-07 23:09:07 +0530

    SetupPy: Initialized

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='py_log_symbols',
+    packages=['py_log_symbols'],
+    version='0.0.1',
+    description='Colored symbols for various log levels for Python',
+    author='Manraj Singh',
+    author_email='manrajsinghgrover@gmail.com',
+    url='https://github.com/ManrajGrover/py-log-symbols',
+    keywords=[
+        'log symbols',
+        'symbols',
+        'log'
+    ]
+)
