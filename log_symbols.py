425fb05dd62977a0d75ec0250bcb02541386af35
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-09-07 23:03:14 +0530

    Log Symbols: Check for support and fallback for unsupported OS

--- log_symbols.py
+++ log_symbols.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from enum import Enum
 from colorama import init, Fore
+import platform
 
 init(autoreset=True)
 
@@ -18,7 +19,22 @@ FALLBACKS = {
     'error': '×'
 }
 
-SYMBOLS = MAIN
+
+def is_supported():
+    os_arch = platform.system() + str(platform.architecture()[0])
+    os_name = platform.system() + platform.release()
+
+    print os_name
+
+    if os_arch is not 'Windows32bit':
+        return True
+
+    if os_name is 'Windows10':
+        return True
+
+    return False
+
+SYMBOLS = MAIN if is_supported() else FALLBACKS
 
 
 class LogSymbols(Enum):
