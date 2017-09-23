599df3915fec8700810aa6831e694e038bb8900f
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-09-23 23:13:03 +0530

    Setup.py: Fixes dependencies issue

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-from distutils.core import setup # pylint: disable=no-name-in-module,import-error
+from setuptools import setup, find_packages # pylint: disable=no-name-in-module,import-error
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+def dependencies(file):
+    with open(file) as f:
+        return f.read().splitlines()
 
 setup(
     name='log_symbols',
-    packages=['log_symbols'],
-    version='0.0.1',
+    packages=find_packages(exclude=('tests',)),
+    version='0.0.4',
     description='Colored symbols for various log levels for Python',
+    long_description=readme(),
     author='Manraj Singh',
     author_email='manrajsinghgrover@gmail.com',
     url='https://github.com/ManrajGrover/py-log-symbols',
@@ -12,5 +21,7 @@ setup(
         'log symbols',
         'symbols',
         'log'
-    ]
+    ],
+    install_requires=dependencies('requirements.txt'),
+    tests_require=dependencies('requirements-dev.txt')
 )
