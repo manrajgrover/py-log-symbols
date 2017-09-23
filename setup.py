7f22b47d294db2252567486bdcbfcfc66bead338
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-09-23 17:05:23 +0530

    Setup: py_log_symbols -> log_symbols

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup # pylint: disable=no-name-in-module,import-error
 
 setup(
-    name='py_log_symbols',
-    packages=['py_log_symbols'],
+    name='log_symbols',
+    packages=['log_symbols'],
     version='0.0.1',
     description='Colored symbols for various log levels for Python',
     author='Manraj Singh',
