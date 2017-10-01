54a7143fb9e7dbc8b3fe7b73396ebf32c38feb0e
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-10-02 01:43:54 +0530

    Setup: Version bumped to 0.0.10

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages # pylint: disable=no-name-in-module,import-error
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
 def dependencies(file):
     with open(file) as f:
         return f.read().splitlines()
@@ -11,10 +7,10 @@ def dependencies(file):
 setup(
     name='log_symbols',
     packages=find_packages(exclude=('tests', 'examples')),
-    version='0.0.9',
+    version='0.0.10',
     license='MIT',
     description='Colored symbols for various log levels for Python',
-    long_description=readme(),
+    long_description='Colored symbols for various log levels for Python. Find the documentation here: https://github.com/ManrajGrover/py-log-symbols.',
     author='Manraj Singh',
     author_email='manrajsinghgrover@gmail.com',
     url='https://github.com/ManrajGrover/py-log-symbols',
