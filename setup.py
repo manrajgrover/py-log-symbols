3e5528dfe794d45802b16f2f3e213b3794ea9d66
Dan Sully <dsully@linkedin.com>
2018-03-11 08:56:39 -0700

    Conditionally include enum34

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-from setuptools import setup, find_packages # pylint: disable=no-name-in-module,import-error
+import sys
+
+from setuptools import setup, find_packages  # pylint: disable=no-name-in-module,import-error
+
+install_requires = []
+
+# Python < 3.4 doesn't have enum34
+if sys.version_info[0:2] < (3, 4):
+    install_requires = ['enum34==1.1.6']
+
 
 def dependencies(file):
     with open(file) as f:
         return f.read().splitlines()
 
+
 setup(
     name='log_symbols',
     packages=find_packages(exclude=('tests', 'examples')),
-    version='0.0.12',
+    version='0.0.13',
     license='MIT',
     description='Colored symbols for various log levels for Python',
     long_description='Colored symbols for various log levels for Python. Find the documentation here: https://github.com/ManrajGrover/py-log-symbols.',
@@ -19,7 +29,7 @@ setup(
         'symbols',
         'log'
     ],
-    install_requires=dependencies('requirements.txt'),
+    install_requires=dependencies('requirements.txt') + install_requires,
     tests_require=dependencies('requirements-dev.txt'),
     include_package_data=True
 )
